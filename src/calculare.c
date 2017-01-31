981f4f2d7ad85b60857bc9032d987fbfad5244db
anonymous <anonymous>
2017-01-31 11:15:15 -0500

    Initial framework set up.  Makefile, and first batch of tests implemented

---
+++ src/calculare.c
@@ -0,0 +1,8 @@
+#include "calculare.h"
+
+int int2roman (int input, char *output)
+{
+	return 0;
+}
+
+
---
+++ src/calculare.h
@@ -0,0 +1,7 @@
+#ifndef _CALCULARE_H_
+#define _CALCULARE_H_
+
+int int2roman (int input, char *output);
+
+
+#endif
