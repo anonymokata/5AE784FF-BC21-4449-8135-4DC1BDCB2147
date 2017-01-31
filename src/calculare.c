541ad603a25a4e2ea7c36bbcab83884d2f2ddbd4
anonymous <anonymous>
2017-01-31 11:59:31 -0500

    Passed first batch of tests for int2roman.  Next: add more thorough tests

--- src/calculare.c
+++ src/calculare.c
@@ -1,66 +1,75 @@
 #include "calculare.h"
+#include <stdio.h>
 
 
 int int2roman (int input, char *output)
 {
+	output[0] = '\0';
+
 	while (input >= 1000)
 	{
 		strncat (output, "M", MAX_STRING);
-		input =- 1000;
+		input -= 1000;
 	}
 
 	while (input >= 500)
 	{
 		strncat (output, "D", MAX_STRING);
-		input =- 500;
+		input -= 500;
 	}
 
 	while (input >= 400)
 	{
 		strncat (output, "CD", MAX_STRING);
-		input =- 400;
+		input -= 400;
 	}
 
 	while (input >= 100)
 	{
 		strncat (output, "C", MAX_STRING);
-		input =- 100;
+		input -= 100;
 	}
 
 	while (input >= 90)
 	{
 		strncat (output, "XC", MAX_STRING);
-		input =- 90;
+		input -= 90;
 	}
 
 	while (input >= 50)
 	{
 		strncat (output, "L", MAX_STRING);
-		input =- 50;
+		input -= 50;
 	}
 
 	while (input >= 40)
 	{
 		strncat (output, "XL", MAX_STRING);
-		input =- 40;
+		input -= 40;
 	}
 
 	while (input >= 10)
 	{
 		strncat (output, "X", MAX_STRING);
-		input =- 10;
+		input -= 10;
 	}
 
-	while (input >= 4)
+	while (input >= 5) 
+	{
+		strncat (output, "V", MAX_STRING);
+		input -= 5;
+	}
+
+	while (input >= 4) 
 	{
 		strncat (output, "IV", MAX_STRING);
-		input =- 4;
+		input -= 4;
 	}
 
 	while (input >= 1)
 	{
 		strncat (output, "I", MAX_STRING);
-		input =- 1;
+		input -= 1;
 	}
 
 	return 0;
