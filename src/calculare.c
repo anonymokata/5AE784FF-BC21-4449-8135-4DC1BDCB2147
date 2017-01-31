99330c915563243fc95ef0674a502a6db392ef04
anonymous <anonymous>
2017-01-31 12:36:27 -0500

    refactored int2roman to cut reduce repeated code

--- src/calculare.c
+++ src/calculare.c
@@ -1,91 +1,25 @@
 #include "calculare.h"
 #include <stdio.h>
 
+#define INT2ROMANDIGIT(value,digit,input,output,max) while (input >= value) { strncat (output, digit,max); input -= value; }
 
 int int2roman (int input, char *output)
 {
 	output[0] = '\0';
 
-	while (input >= 1000)
-	{
-		strncat (output, "M", MAX_STRING);
-		input -= 1000;
-	}
-
-	while (input >= 900)
-	{
-		strncat (output, "CM", MAX_STRING);
-		input -= 900;
-	}
-
-	while (input >= 500)
-	{
-		strncat (output, "D", MAX_STRING);
-		input -= 500;
-	}
-
-	while (input >= 400)
-	{
-		strncat (output, "CD", MAX_STRING);
-		input -= 400;
-	}
-
-	while (input >= 100)
-	{
-		strncat (output, "C", MAX_STRING);
-		input -= 100;
-	}
-
-	while (input >= 90)
-	{
-		strncat (output, "XC", MAX_STRING);
-		input -= 90;
-	}
-
-	while (input >= 50)
-	{
-		strncat (output, "L", MAX_STRING);
-		input -= 50;
-	}
-
-	while (input >= 40)
-	{
-		strncat (output, "XL", MAX_STRING);
-		input -= 40;
-	}
-
-	while (input >= 10)
-	{
-		strncat (output, "X", MAX_STRING);
-		input -= 10;
-	}
-
-	while (input >= 9)
-	{
-		strncat (output, "IX", MAX_STRING);
-		input -= 9;
-	}
-
-	while (input >= 5) 
-	{
-		strncat (output, "V", MAX_STRING);
-		input -= 5;
-	}
-
-	while (input >= 4) 
-	{
-		strncat (output, "IV", MAX_STRING);
-		input -= 4;
-	}
-
-	while (input >= 1)
-	{
-		strncat (output, "I", MAX_STRING);
-		input -= 1;
-	}
+	INT2ROMANDIGIT (1000, "M",  input, output, MAX_STRING);
+	INT2ROMANDIGIT (900,  "CM", input, output, MAX_STRING);
+	INT2ROMANDIGIT (500,  "D",  input, output, MAX_STRING);
+	INT2ROMANDIGIT (400,  "CD", input, output, MAX_STRING);
+	INT2ROMANDIGIT (100,  "C",  input, output, MAX_STRING);
+	INT2ROMANDIGIT (90,   "XC", input, output, MAX_STRING);
+	INT2ROMANDIGIT (50,   "L",  input, output, MAX_STRING);
+	INT2ROMANDIGIT (40,   "XL", input, output, MAX_STRING);
+	INT2ROMANDIGIT (10,   "X",  input, output, MAX_STRING);
+	INT2ROMANDIGIT (9,    "IX", input, output, MAX_STRING);
+	INT2ROMANDIGIT (5,    "V",  input, output, MAX_STRING);
+	INT2ROMANDIGIT (4,    "IV", input, output, MAX_STRING);
+	INT2ROMANDIGIT (1,    "I",  input, output, MAX_STRING);
 
 	return 0;
-
 }
-
-
