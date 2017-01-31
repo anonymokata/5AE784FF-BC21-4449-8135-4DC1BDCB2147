0f1175da6cce5f6ba492bccc347d68d697a1a705
anonymous <anonymous>
2017-01-31 11:25:07 -0500

    Initial implementation of int2roman - passe some subtests, but barfs on IV, so far

--- src/calculare.c
+++ src/calculare.c
@@ -1,8 +1,70 @@
 #include "calculare.h"
 
+
 int int2roman (int input, char *output)
 {
+	while (input >= 1000)
+	{
+		strncat (output, "M", MAX_STRING);
+		input =- 1000;
+	}
+
+	while (input >= 500)
+	{
+		strncat (output, "D", MAX_STRING);
+		input =- 500;
+	}
+
+	while (input >= 400)
+	{
+		strncat (output, "CD", MAX_STRING);
+		input =- 400;
+	}
+
+	while (input >= 100)
+	{
+		strncat (output, "C", MAX_STRING);
+		input =- 100;
+	}
+
+	while (input >= 90)
+	{
+		strncat (output, "XC", MAX_STRING);
+		input =- 90;
+	}
+
+	while (input >= 50)
+	{
+		strncat (output, "L", MAX_STRING);
+		input =- 50;
+	}
+
+	while (input >= 40)
+	{
+		strncat (output, "XL", MAX_STRING);
+		input =- 40;
+	}
+
+	while (input >= 10)
+	{
+		strncat (output, "X", MAX_STRING);
+		input =- 10;
+	}
+
+	while (input >= 4)
+	{
+		strncat (output, "IV", MAX_STRING);
+		input =- 4;
+	}
+
+	while (input >= 1)
+	{
+		strncat (output, "I", MAX_STRING);
+		input =- 1;
+	}
+
 	return 0;
+
 }
 
 
--- src/calculare.h
+++ src/calculare.h
@@ -1,6 +1,10 @@
 #ifndef _CALCULARE_H_
 #define _CALCULARE_H_
 
+#include <string.h>
+
+#define MAX_STRING 256
+
 int int2roman (int input, char *output);
 
 
