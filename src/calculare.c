b1d7658ed3331cae988a6fe1dc4ffdbfd2eb44ea
anonymous <anonymous>
2017-01-31 13:27:38 -0500

    minor readability cleanup and function descriptions

--- src/calculare.c
+++ src/calculare.c
@@ -3,8 +3,13 @@
 
 #define INT2ROMANDIGIT(value,digit,input,output,max) while (input >= value) { strncat (output, digit,max); input -= value; }
 
+/* 
+  int2roman converts the value passed as 'input' to Roman Numeral
+  and places the result int the character buffer given by 'output'
+*/
 int int2roman (int input, char *output)
 {
+
 	output[0] = '\0';
 
 	INT2ROMANDIGIT (1000, "M",  input, output, MAX_STRING);
@@ -24,11 +29,16 @@ int int2roman (int input, char *output)
 	return 0;
 }
 
+/*
+	roman2int converts the roman numeral passed in via the character 
+	buffer 'input' to decimal integer, and returns it, or -1 in the 
+	case of an error
+*/
 int roman2int (char *input)
 {
-	int i = 0;
-  int output = 0;
-	int value = 0;
+	int i          = 0;
+  int output     = 0;
+	int value      = 0;
 	int last_value = 10000;
 
   while ((i<MAX_STRING)&&(input[i] != '\0'))
@@ -36,13 +46,13 @@ int roman2int (char *input)
 		switch (input[i])
 		{
 			case 'M': value = 1000; break;
-			case 'D': value = 500; break;
-			case 'C': value = 100; break;
-			case 'L': value = 50; break;
-			case 'X': value = 10; break;
-			case 'V': value = 5; break;
-			case 'I': value = 1; break;
-		  default: return -1; break;
+			case 'D': value = 500;  break;
+			case 'C': value = 100;  break;
+			case 'L': value = 50;   break;
+			case 'X': value = 10;   break;
+			case 'V': value = 5;    break;
+			case 'I': value = 1;    break;
+		  default: return -1;     break;
 		}
 		output += value;
 		if (value > last_value)
